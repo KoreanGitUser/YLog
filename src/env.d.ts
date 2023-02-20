464090d0318b1003fd98c2f9dd33af4f3ba34781
장용호 <jyh990605@gmail.com>
2023-02-20 20:22:28 +0900

    env: add DB_URL api

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly API_BASE_URL: string;
   readonly FIREBASE_API_KEY: string;
   readonly FIREBASE_AUTH_DOMAIN: string;
+  readonly FIREBASE_DB_URL: string;
   readonly FIREBASE_PROJECT_ID: string;
   readonly FIREBASE_STORAGE_BUCKET: string;
   readonly FIREBASE_MESSAGING_SENDER_ID: string;
