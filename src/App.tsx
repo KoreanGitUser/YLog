da9acea1ae89045d10358deea19766a1be143d2f
장용호 <jyh990605@gmail.com>
2023-02-22 15:29:18 +0900

    canvas: learn canvas tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
 import UnAuthRoutes from "@components/routes/UnAuthRoutes";
 
 const App = () => {
+  useEffect(() => {}, []);
+
   return (
     <div>
       <UnAuthRoutes />
