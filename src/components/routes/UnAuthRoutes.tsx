297fbe55d4b04e622a1cd947e17c3051fae8ff72
장용호 <jyh990605@gmail.com>
2023-02-28 14:06:54 +0900

    rotues: add & apply sign-up routes

--- src/components/routes/UnAuthRoutes.tsx
+++ src/components/routes/UnAuthRoutes.tsx
@@ -1,14 +1,16 @@
+import SignUp from "@components/auth/SignUp";
 import LoginPage from "@pages/unAuth/LoginPage";
 import PATH from "@utils/routes/PATH";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-const { LOGIN } = PATH;
+const { LOGIN, SIGN_UP } = PATH;
 
 const UnAuthRoutes = () => {
   return (
     <Routes>
       <Route path={LOGIN} element={<LoginPage />} />
-      <Route path="*" element={<Navigate replace to={LOGIN} />} />
+      <Route path={SIGN_UP} element={<SignUp />} />
+      <Route path="*" element={<Navigate replace to={SIGN_UP} />} />
     </Routes>
   );
 };
